cfffa8a0a9916a4d74097bf7e77b5f39bcb4eceb
Amlan <sahoo.amlanranjan@gmail.com>
2024-07-05 23:45:27 +0530

    Add comments to code

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-import inquirer from "inquirer";
-import qr from "qr-image";
-import fs from "node:fs";
+// Import necessary modules
+import inquirer from "inquirer"; // Inquirer is used to prompt user input in the command line
+import qr from "qr-image"; // qr-image is used to generate QR codes
+import fs from "node:fs"; // File system module to handle file operations
 
+// Define the questions to prompt the user
 const questions = [
     {
-      type: 'input',
-      name: 'url',
-      message: "Enter the URL: ",
+        type: 'input',
+        name: 'url',
+        message: "Enter the URL: ", // Message displayed to the user
     }
-]
+];
 
+// Prompt the user to enter the URL
 inquirer.prompt(questions).then((answers) => {
+    // Get the user input from the answers
     let myURL = answers['url'];
-    console.log(myURL);
-    var qr_svg = qr.image(myURL, { type: 'png' });
-    qr_svg.pipe(fs.createWriteStream('qr_code.png'));
-  });
+    console.log(myURL); // Log the URL to the console
 
+    // Generate a QR code from the URL
+    var qr_svg = qr.image(myURL, { type: 'png' });
+
+    // Pipe the generated QR code to a file named 'qr_code.png'
+    qr_svg.pipe(fs.createWriteStream('qr_code.png'));
+});
